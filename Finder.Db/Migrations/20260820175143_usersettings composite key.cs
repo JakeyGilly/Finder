54ce72df2f3e6decd416204c0ee4971998297a42
Jake Gillman <jakegillman3@gmail.com>
2026-08-21 00:24:34 +0100

    migration

---
+++ Finder.Db/Migrations/20260820175143_usersettings composite key.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finder.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class usersettingscompositekey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserSettings",
+                table: "UserSettings");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserSettings",
+                table: "UserSettings",
+                columns: new[] { "UserId", "Setting" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserSettings",
+                table: "UserSettings");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserSettings",
+                table: "UserSettings",
+                column: "UserId");
+        }
+    }
+}
