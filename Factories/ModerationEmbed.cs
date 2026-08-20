9507bb9cc453fa0a505ac720aa03f8403f635b96
Jake Gillman <jakegillman3@gmail.com>
2026-08-20 03:45:30 +0100

    finished timers for unban

--- Factories/ModerationEmbed.cs
+++ Factories/ModerationEmbed.cs
@@ -4,7 +4,7 @@ using Discord.WebSocket;
 namespace Finder.Bot.Factories;
 
 public class ModerationEmbedFactory {
-    public EmbedBuilder BuildEmbed(string title, SocketGuildUser? user = null, string? reason = null, string? time = null) {
+    public EmbedBuilder BuildEmbed(string title, IUser? user = null, string? reason = null, string? time = null) {
         var embed = new EmbedBuilder { Title = title, Footer = new EmbedFooterBuilder { Text = "FinderBot" } };
         if (user != null) embed.AddField("User", $"{user.Mention} ({user.Username})", false);
         if (reason != null && reason != "No reason given.") embed.AddField("For reason", reason, false);
--- Program.cs
+++ Program.cs
@@ -27,10 +27,10 @@ class Program {
         await handler.InitializeAsync();
         client.Log += LoggingService.LogAsync;
         commands.Log += LoggingService.LogAsync;
-        // UnBanMuteTimer.StartTimer(client, services.GetRequiredService<IUnitOfWork>());
+        new UnBanMuteTimer(client, services).StartTimer();
         new CountdownTimer(client, services).StartTimer();
         client.ReactionAdded += TicTacToeModule.OnReactionAddedEvent;
-        // client.ReactionAdded += new ModerationModule(services.GetRequiredService<IUnitOfWork>()).OnReactionAddedEvent;
+        client.ReactionAdded += new ModerationModule(services.GetRequiredService<IUnitOfWork>()).OnReactionAddedEvent;
         client.ButtonExecuted += new PollModule(services.GetRequiredService<IUnitOfWork>()).OnButtonExecutedEvent;
         client.ButtonExecuted += new TicketingModule(services.GetRequiredService<IUnitOfWork>()).OnButtonExecutedEvent;
         client.MessageReceived += new LevellingModule(services.GetRequiredService<IUnitOfWork>()).OnMessageReceivedEvent;
@@ -39,7 +39,7 @@ class Program {
         await Task.Delay(Timeout.Infinite);
     }
     
-    private static readonly DiscordSocketConfig discordConfig = new() {
+    private static readonly DiscordSocketConfig DiscordConfig = new() {
         GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers | GatewayIntents.GuildEmojis
     };
     
@@ -47,7 +47,7 @@ class Program {
         return new ServiceCollection()
             .AddDbContext<BotDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("PostgreSQL")))
             .AddScoped<IUnitOfWork, UnitOfWork>()
-            .AddSingleton<DiscordShardedClient>(x => new DiscordShardedClient(discordConfig))
+            .AddSingleton<DiscordShardedClient>(x => new DiscordShardedClient(DiscordConfig))
             .AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordShardedClient>()))
             .AddSingleton<InteractionHandler>()
             .BuildServiceProvider();
